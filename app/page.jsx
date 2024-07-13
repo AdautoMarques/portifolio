3fb2a95366fd829a6f30e65409fa8ab3bcccbfd3
AdautoMarques <adautomarques937@gmail.com>
2024-07-12 22:45:11 -0300

    Iniciando a pagina Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
-
+import { Button } from "@/components/ui/button";
+import { FiDownload } from 'react-icons/fi'
 
 export default function Home() {
   return (
-    <main>Homepage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span>Desenvolvedor Front-End</span>
+            <h1>
+              Olá, eu sou <br />
+              <span>Adauto Marques</span>
+            </h1>
+          </div>
+          <div>Foto</div>
+        </div>
+      </div>
+    </section>
   );
 }
